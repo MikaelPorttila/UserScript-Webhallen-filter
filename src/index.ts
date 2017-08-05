10c30c02338afe400eb9384e3eb0c02ffd7ad424
Mikael Porttila <mikael.porttila@gmail.com>
2017-08-05 11:32:19 +0200

    Removed style file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-// Include custom styles into the script to be injected into web pages.
-// Remove this line to reduce script output size if you do not need styles.
-import './index.css';
-
 export class FilterScript {
 
     constructor(){}
